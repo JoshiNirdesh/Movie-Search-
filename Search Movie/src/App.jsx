06b98fe0d0d2b54098c2ebbf34db28bddc026663
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-07-14 12:47:37 +0545

    routing

--- Search Movie/src/App.jsx
+++ Search Movie/src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import MovieCard from "./components/MovieCard";
+import Navbar from "./components/Navbar";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
-  const movienum = 1;
-
   return (
     <>
-      <Home />
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </>
   );
 }
